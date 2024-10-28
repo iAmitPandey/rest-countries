63ebd773ffa86cab405a19de3efc065ff4c143e5
AMIT PANDEY <122152274+iAmitPandey@users.noreply.github.com>
2024-10-28 11:01:04 +0530

    Add files via upload

---
+++ src/utils/countryFilter.js
@@ -0,0 +1,34 @@
+export const  countryFilter = (countryData, selectedRegion, selectedSubRegion, searchValue, sortingType)=>{
+    const filterDetails = countryData
+    .filter((country) => {
+      if (selectedRegion) {
+        return country.region === selectedRegion;
+      }
+      return true;
+    })
+    .filter((country) => {
+      if (selectedSubRegion) {
+        return country.subregion === selectedSubRegion;
+      }
+      return true;
+    })
+    .filter((country) => {
+      const countryName = country.name.common.toLowerCase();
+      return countryName.includes(searchValue);
+      // return countryName.startsWith(searchValue);
+    })
+    .sort((firstCountry, secondCountry) => {
+      if (sortingType === "Ascending Area") {
+        return firstCountry.area - secondCountry.area;
+      } else if (sortingType === "Descending Area") {
+        return secondCountry.area - firstCountry.area;
+      } else if (sortingType === "Ascending Population") {
+        return firstCountry.population - secondCountry.population;
+      } else if (sortingType === "Descending Population") {
+        return secondCountry.population - firstCountry.population;
+      }
+      return true;
+    });
+
+    return filterDetails
+}
